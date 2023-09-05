45f859f7dabe658e6aa83d0523fc1624d930f13c
Nerd <imalonelynerd@gmail.com>
2023-09-05 15:50:09 +0200

    2023.09.05.2

--- index.php
+++ index.php
@@ -16,15 +16,19 @@
     <link rel="icon" href="icon/favicon.ico" sizes="any">
     <link rel="icon" href="icon/favicon.svg" type="image/svg+xml">
     <link rel="manifest" href="/manifest.webmanifest">
-    <link rel="stylesheet" type="text/css" href="./styles/main.css">
+    <link rel="stylesheet" type="text/css" href="styles/tt.css">
 </head>
 
 <body>
 <div id="date">
-    <p id="day_num"><?php
-        date_default_timezone_set('Europe/Paris');
-        echo date('d');
-        ?></p>
+    <p id="day_num">
+        <?php
+
+            date_default_timezone_set('Europe/Paris');
+            echo date('d');
+
+        ?>
+    </p>
     <p id="mouth"><?php echo date("D") . "<br>" . date("F Y") ?></p>
     <div id="day_rela">
         <a>Today</a>
@@ -41,14 +45,14 @@
         error_reporting(E_ALL);
 
         include("./utils/ttWrapper.php");
-        $edt = new Timetable("https://adecampus.univ-rouen.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=31782&projectId=0&calType=ical&nbWeeks=1&displayConfigId=8");
-        $edt->fetchCal();
+        $tt = new Timetable("https://adecampus.univ-rouen.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=31782&projectId=0&calType=ical&nbWeeks=1&displayConfigId=8");
+        $tt->fetchCal();
         ?>
         <div id="days">
             <?php
             for ($j = 0; $j < 6; $j += 1) {
-                print_r($edt->getDay($j));
-                $dateTime = $edt->getDate($edt->getDay($j));
+                print_r($tt->getDay($j));
+                $dateTime = $tt->getDate($tt->getDay($j));
                 echo "<p class='day'>" . substr($dateTime->format("D"), 0, 1) . "<br><span>" . $dateTime->format("d") . "</span></p>\n";
             }
             ?>
@@ -56,33 +60,33 @@
     </div>
     <div id="courses">
         <?php
-        for($j = 0; $j < 6; $j += 1){
-            $dateTime = $edt->getDate($edt->getDay($j));
+        for ($j = 0; $j < 6; $j += 1) {
+            $dateTime = $tt->getDate($tt->getDay($j));
             echo "<div class='course'>\n";
-            echo "<p class='date'>" . $edt->getFormatedDate($edt->getDay($j),"l d M") . "</p>\n";
+            echo "<p class='date'>" . $tt->getFormatedDate($tt->getDay($j), "l d M") . "</p>\n";
             echo "<div class='sb'>\n";
             echo "<div class='time'>\n";
-            foreach ($edt->getDay($j) as $event) {
+            foreach ($tt->getDay($j) as $event) {
                 echo "<div class='time-se'>\n";
-                echo "<p class='start'>" . $edt->getHours($event)[0] . "</p>\n";
-                echo "<p class='end'>" . $edt->getHours($event)[1] . "</p>\n";
+                echo "<p class='start'>" . $tt->getHours($event)[0] . "</p>\n";
+                echo "<p class='end'>" . $tt->getHours($event)[1] . "</p>\n";
                 echo "</div>\n";
             }
             echo "</div>\n";
             echo "<div class='bar'></div>\n";
             echo "<div class='course-block'>\n";
-            foreach ($edt->getDay($j) as $event) {
+            foreach ($tt->getDay($j) as $event) {
                 echo "<div class='course-sb ";
-                if (date("U") <= $edt->getDateFromEvent($event)->format("U") && date("U") >= $edt->getDateFromEvent($event)->format("U")) {
+                if (date("U") <= $tt->getDateFromEvent($event)->format("U") && date("U") >= $tt->getDateFromEvent($event)->format("U")) {
                     echo "select-c";
                 }
                 echo "'>\n";
-                echo "<p class='name'>" . $edt->getTitle($event) . "</p>\n";
-                preg_match("/^(\w[a-zA-Z@#0-9.]* \w[a-zA-Z@#0-9.]*) \(.*\) ?([A-Z]{0,1})?.*$/", $edt->getLocation($event), $matchs);
+                echo "<p class='name'>" . $tt->getTitle($event) . "</p>\n";
+                preg_match("/^(\w[a-zA-Z@#0-9.]* \w[a-zA-Z@#0-9.]*) \(.*\) ?([A-Z]{0,1})?.*$/", $tt->getLocation($event), $matchs);
                 array_shift($matchs);
                 $string = implode(" ", $matchs);
                 echo "<p class='where'><img alt='location' src='./styles/location.svg'>" . $string . "</p>\n";
-                echo "<p class='prof'>" . $edt->getGroup($event) . "</p>\n";
+                echo "<p class='prof'>" . $tt->getGroup($event) . "</p>\n";
                 echo "</div>\n";
             }
             echo "</div>\n";
